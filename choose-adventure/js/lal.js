985e7a38667af3408bde2538c91bbcbc123a2938
akileshswamy <aswamy@eastsideprep.org>
2021-12-14 14:21:03 -0800

    template code

--- choose-adventure/js/lal.js
+++ choose-adventure/js/lal.js
@@ -1,1 +1,2 @@
-alert("Welcome to your Disneyland Adventure! Choose your tour guide! LeMickey or ADisney?");
+//Intro. Question
+let tourGuide = prompt("Welcome to your Disneyland Adventure! Choose your tour guide! LeMickey or ADisney?");
