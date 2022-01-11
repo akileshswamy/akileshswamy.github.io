b50955dbf6068979b7e5975adae5332a8f864f7e
akileshswamy <aswamy@eastsideprep.org>
2022-01-11 14:59:07 -0800

    tiktok assignment

--- TikTok/js/tiktok.js
+++ TikTok/js/tiktok.js
@@ -1,11 +1,11 @@
 //TikTok's Magic Algorithm
-function algorithm(likes, comment) {
+function algorithm(likes, comment, views, shares) {
     let rank = 0;
 
     //magic!
-    if (likes >= 10000 && comment.includes("basketball")) {
+    if (likes >= 10000 && comment.includes("basketball") && views >= 100000 && shares >= 100) {
         rank = 1;
-    } else if (likes >= 500 && comment.includes("football")) {
+    } else if (likes >= 500 && comment.includes("football") && views >= 5000 && shares >= 5) {
         rank = 100;
     } else {
         rank = 1000
@@ -15,11 +15,11 @@ function algorithm(likes, comment) {
 
 alert("Welcome to TikTok! Imagine I'm showing you a tiktok video right now");
 let likeCount = Number(prompt("How many likes did this Tiktok video get?"));
-// ask for a video comment
 let topComment = prompt("What is the top comment on this TikTok video?");
-
-//using the two inputs above, pass that into the magic function and return the result
-let rankResult = magicAlgorithm(likeCount, topComment);
+let numViews = Number(prompt("How many views did this Tiktok videeo get?"))
+let numShares = Number(prompt("How many shares did this Tiktok video get?"))
+    //using the two inputs above, pass that into the magic function and return the result
+let rankResult = algorithm(likeCount, topComment, numViews, numShares);
 let resultString = "The video's rank: " + rankResult;
 
 alert(resultString);
