33a34a255b83a005e8537c36a4a4c2ec28dbaab8
akileshswamy <aswamy@eastsideprep.org>
2022-01-13 09:36:19 -0800

    tiktok html game

--- TikTok/js/tiktok.js
+++ TikTok/js/tiktok.js
@@ -13,13 +13,32 @@ function algorithm(likes, comment, views, shares) {
     return rank;
 }
 
-alert("Welcome to TikTok! Imagine I'm showing you a tiktok video right now");
-let likeCount = Number(prompt("How many likes did this Tiktok video get?"));
-let topComment = prompt("What is the top comment on this TikTok video?");
-let numViews = Number(prompt("How many views did this Tiktok videeo get?"))
-let numShares = Number(prompt("How many shares did this Tiktok video get?"))
-    //using the two inputs above, pass that into the magic function and return the result
-let rankResult = algorithm(likeCount, topComment, numViews, numShares);
-let resultString = "The video's rank: " + rankResult;
+function popup() {
+    alert("Welcome to TikTok! Imagine I'm showing you a tiktok video right now");
+    let likeCount = Number(prompt("How many likes did this Tiktok video get?"));
+    let topComment = prompt("What is the top comment on this TikTok video?");
+    let numViews = Number(prompt("How many views did this Tiktok videeo get?"))
+    let numShares = Number(prompt("How many shares did this Tiktok video get?"))
+        //using the two inputs above, pass that into the magic function and return the result
+    let rankResult = algorithm(likeCount, topComment, numViews, numShares);
+    let resultString = "The video's rank: " + rankResult;
 
-alert(resultString);
+    alert(resultString);
+}
+
+
+
+function showUI() {
+    //grab values from text boxes
+    let likeCount = Number(document.getElementById("like-textbox").value);
+    let topComment = document.getElementById("comment-textbox").value;
+    let numViews = Number(document.getElementById("views-textbox").value);
+    let numShares = Number(document.getElementById("shares-textbox").value);
+
+    //do the algorithm
+    let rankResult = algorithm(likeCount, topComment, numViews, numShares);
+
+    //display the output
+    document.getElementById("result-paragraph").innerHTML = "TikTok Rank: " + rankResult;
+
+}
