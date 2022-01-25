e9f23de21e9c486d03f574d7a9aae6fdb20a5e90
akileshswamy <aswamy@eastsideprep.org>
2022-01-25 14:47:52 -0800

    html update

--- vpet/js/pet.js
+++ vpet/js/pet.js
@@ -3,13 +3,13 @@ let lebron = 0;
 let anthony = 0;
 
 function russFunction() {
-    console.log("Russ's Performance: ");
+    console.log("Russ's Performance... ");
 }
 
 function lebronFunction() {
-    console.log("LeBron's Performance: ");
+    console.log("LeBron's Performance... ");
 }
 
 function davisFunction() {
-    console.log("AD's Performance: ")
+    console.log("AD's Performance... ")
 }
