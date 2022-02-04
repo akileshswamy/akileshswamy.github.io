c60420ce272aa0f86e202677c6bd303044515328
akileshswamy <aswamy@eastsideprep.org>
2022-02-03 19:03:20 -0800

    master of canvas

---
+++ canvas-basics/js/canvas.js
@@ -0,0 +1,42 @@
+// count 1 - 10
+for (let t = 1; t < 11; t++) {
+    console.log("The number is " + t);
+}
+// count 10 - 1
+for (let t = 10; t > 0; t--) {
+    console.log("The number is " + t);
+}
+// count 1 - 10 only odd numbers
+for (let t = 1; t < 11; t = t + 2) {
+    console.log("The number is " + t);
+}
+
+// get canvas element
+let myCanvas = document.getElementById("soren");
+// create canvas object
+let ctx = myCanvas.getContext("2d");
+
+//draw rect
+ctx.fillStyle = "LightCoral"
+ctx.fillRect(0, 0, 200, 100);
+
+//draw lines
+ctx.beginPath();
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 100);
+ctx.stroke()
+
+ctx.beginPath
+ctx.moveTo(200, 0);
+ctx.lineTo(0, 100);
+ctx.stroke()
+
+s
+
+for (let x = 25; x > 0; x = x - 5) {
+    ctx.beginPath();
+    ctx.arc(100, 50, x, 0, 2 * Math.PI);
+    ctx.strokeStyle = "Blue";
+    ctx.stroke();
+}
+ctx.arc()
